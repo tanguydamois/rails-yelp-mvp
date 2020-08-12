b71ecdf3b70a651c50c34f755ab9895f5dd08d9c
tanguydamois <tdamois@gmail.com>
2020-08-11 21:37:22 -0400

    seed unsuccessfully

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+fish_bone = { name: "Ficsh Bone", address: "420 Notre-Dame Ouest Local 4 Montréal, QC H2Y 1V3", phone_number: '438 584 1254' }
+rib_n_reef_steakhouse =  { name: "Rib n Reef Steakhouse", address: "8105 Decarie Blvd Montréal, QC H4P 2H5", phone_number: '514 584 3697' }
+
+[ fish_bone, rib_n_reef_steakhouse ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
