180d3d272b57778860ced6c99a4c531c83a66169
tanguydamois <tdamois@gmail.com>
2020-08-13 21:36:42 -0400

    def show create

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,13 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def create
+    @restaurant = Restaurant.new
+  end
 end
